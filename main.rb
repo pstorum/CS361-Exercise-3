8adb0e7ffb35bf34e2a5b3205053d04d83a166b3
Peter Storum <p-storum@hotmail.com>
2021-10-17 12:17:05 -0700

    removed Demeter violations
    removed Demeter violations... maybe

--- main.rb
+++ main.rb
@@ -1,16 +1,11 @@
 class Student
-
-  def remove_from_schedule(course, schedule, quarter_id)
-    quarter = schedul.quarter[quarter_id]
-    student_courses = quarter.course_list
-    student_courses.remove(course)
+  MAX_COURSES = 5
+  def remove_from_schedule(course_list)
+    course_list.remove(course)
   end
 
-  def add_to_schedule(course, schedule, quarter_id)
-    quarter = schedule.quarter[quarter_id]
-    course_list = quarter.course_list
-    max_courses = course_list.maximum_number_of_courses
-    if max_courses < 5
+  def add_to_schedule(course_list, maximum_number_of_courses)
+    if maximum_number_of_courses < MAX_COURSES
       course_list.add(course)
     end
   end
